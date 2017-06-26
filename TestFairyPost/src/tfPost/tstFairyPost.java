2229fd776cdc562ae1c5b52f714bc0c5b24d6333
Q3188 <Q3188@QKNBK376.Corp.qualitykiosk.com>
2017-06-26 15:30:21 +0530

    Test commit

---
+++ TestFairyPost/src/tfPost/tstFairyPost.java
@@ -0,0 +1,65 @@
+package tfPost;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class tstFairyPost {
+
+	private final String USER_AGENT = "Mozilla/5.0";
+
+	public static void main(String[] args) throws Exception {
+
+		tstFairyPost http = new tstFairyPost();
+
+		System.out.println("\nTesting 1 - Send Http POST request");
+		http.sendPost();
+
+	}
+	// HTTP POST request
+	private void sendPost() throws Exception {
+
+		String url = "https://upload.testfairy.com/api/upload";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		//add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+		String urlParameters = "api_key=aa2f450c873d306064c9e227872fab23fac448ec&file=@ContactManager.apk&uploaded_via=api";
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+
+}
